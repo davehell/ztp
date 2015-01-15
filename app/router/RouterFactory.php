bcdc42ed2713f2c255a62d69656e6f851be736ed
dhe <dhe@instar.cz>
2015-01-15 11:40:37 +0100

    do routeru pridana moznost ssl

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -15,11 +15,12 @@ class RouterFactory
   /**
    * @return \Nette\Application\IRouter
    */
-  public function createRouter()
+  public function createRouter($isSecured = false)
   {
+    $flags = ($isSecured ? Route::SECURED : 0);
     $router = new RouteList();
     //$router[] = new Route('<presenter>[/<verzeId>]/<action>[/<id>]', 'Verze:seznam');
-    $router[] = new Route('<presenter>/<action> ? verze=<verzeId>', 'Verze:default');
+    $router[] = new Route('<presenter>/<action> ? verze=<verzeId>', 'Verze:default', $flags);
     return $router;
   }
 
