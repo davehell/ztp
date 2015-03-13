573a28ffe23c34234cf5bb46d37d26230f2f0eeb
dhe <dhe@instar.cz>
2015-03-13 14:47:09 +0100

    deploy skript

---
+++ deploy.bat
@@ -0,0 +1,26 @@
+rd /s /q "../ztp-ostra/app/components"
+mkdir "../ztp-ostra/app/components"
+xcopy /s /y "app/components" "../ztp-ostra/app/components"
+
+rd /s /q "../ztp-ostra/app/model"
+mkdir "../ztp-ostra/app/model"
+xcopy /s /y "app/model" "../ztp-ostra/app/model"
+
+rd /s /q "../ztp-ostra/app/presenters"
+mkdir "../ztp-ostra/app/presenters"
+xcopy /s /y "app/presenters" "../ztp-ostra/app/presenters"
+
+rd /s /q "../ztp-ostra/app/router"
+mkdir "../ztp-ostra/app/router"
+xcopy /s /y "app/router" "../ztp-ostra/app/router"
+
+rd /s /q "../ztp-ostra/app/templates"
+mkdir "../ztp-ostra/app/templates"
+xcopy /s /y "app/templates" "../ztp-ostra/app/templates"
+
+rd /s /q "../ztp-ostra/www"
+mkdir "../ztp-ostra/www"
+xcopy /s /y "www" "../ztp-ostra/www"
+
+rd /s /q "../ztp-ostra/temp"
+mkdir "../ztp-ostra/temp"
