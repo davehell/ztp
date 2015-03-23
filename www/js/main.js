f424562b18589d8b65ec48f85f1a6a813fdfa26d
dhe <dhe@instar.cz>
2015-03-23 14:06:36 +0100

    zmenena struktura main.js

--- www/js/main.js
+++ www/js/main.js
@@ -1,8 +1,3 @@
-function zvyraznitZmenu (zmena) {
-  zmena.addClass( 'zvyraznena' );
-  setTimeout(function() {zmena.removeClass('zvyraznena')}, 5000);
-}
-
 $( document ).ready(function() {
   //barevné zvýraznění změny, která je zadána v url
   var hash = window.location.hash; //např.: "#z115"
@@ -11,7 +6,6 @@ $( document ).ready(function() {
     zvyraznitZmenu(zmena);
   }
 });
-
 
 /**
  * odeslání formuláře pomocí ctrl + enter
@@ -29,102 +23,105 @@ $.fn.enterKey = function (fnc, mod) {
 $('textarea').enterKey(function() {$(this).closest('form').submit(); }, 'ctrl');
 
 
+function zvyraznitZmenu (zmena) {
+  zmena.addClass( 'zvyraznena' );
+  setTimeout(function() {zmena.removeClass('zvyraznena')}, 5000);
+}
+
+/**
+ * Po výběru verze v select boxu v menu se otevře stránka s danou verzí.
+ */
+$('#menuForm #vyberVerze, #menuForm #vyberUzivatele, #menuForm #vyberProtokolu').change(function(event) {
+  var id = $( this ).val();
+  if(!id) return;
+  $('#menuForm').submit();
+});
+
+/**
+ * Posunutí změny nahoru
+ */
+$('.posunNahoru').click(function() {
+  var zmena = $(this).closest('.zmena');
+  zvyraznitZmenu(zmena);
+  var predchudce = zmena.prevAll('.zmena:first')
+  if(predchudce.length) {
+    predchudce.before(zmena);
+    ulozitPoradi();
+  }
+});
+
+/**
+ * Posunutí změny dolů
+ */
+$('.posunDolu').click(function() {
+  var zmena = $(this).closest('.zmena');
+  zvyraznitZmenu(zmena);
+  var naslednik = zmena.nextAll('.zmena:first')
+  if(naslednik.length) {
+    naslednik.after(zmena);
+    ulozitPoradi();
+  }
+});
+
+/**
+ * Uložení pořadí změn v protokolu
+ */
+function ulozitPoradi() {
+  var poradi = new Array();
+  var url = $('ul#zmeny').data('url');
+  $( '.zmena' ).each(function() {
+    poradi.push($(this).data('id'));
+  });
+
+  $.get(url, { seznam: poradi.join() });
+}
+
+/**
+ * Zobrazení formuláře pro zadání výsledku testování změny.
+ */
+$('button.zmenaFunguje').click(function(event) {
+  var zmena = $(this).closest('.zmena');
+  $('#frm-testForm-id').val(zmena.data('id'));
+  var vysledek = zmena.find('.vysledekTestu').html();
+  $('#frm-testForm-vysledek_testu').val(vysledek ? vysledek : 'bez připomínek');
+  $('#modalZmenaFunguje').modal('show');
+  zvyraznitZmenu(zmena);
+});
+
+/**
+ * Vyplnění pole pro výsledek textu na základě zvolené varianty ze seznamu.
+ */
+$('#modalZmenaFunguje li button').click(function(event) {
+  var text = $(this).data('text');
+  $('#frm-testForm-vysledek_testu').val(text);
+});
 
 $(function () {
   $.nette.ext({
-      load: function() {
-        /**
-         * Potvrzovací dialog při kliknutí na odkaz pro smazání.
-         */
-        $('.btn-danger').click(function(event) {
-          event.preventDefault();
-          var url = $(this).attr('href');
-          var confirm_box = confirm('Opravdu?');
-          if(confirm_box) {
-            window.location = url;
-          }
-        });
-
-        /**
-         * Zobrazení formuláře pro zadání výsledku testování změny.
-         */
-        $('button.zmenaFunguje').click(function(event) {
-          var zmena = $(this).closest('.zmena');
-          $('#frm-testForm-id').val(zmena.data('id'));
-          var vysledek = zmena.find('.vysledekTestu').html();
-          $('#frm-testForm-vysledek_testu').val(vysledek ? vysledek : 'bez připomínek');
-          $('#modalZmenaFunguje').modal('show');
-          zvyraznitZmenu(zmena);
-        });
-
-        /**
-         * Vyplnění pole pro výsledek textu na základě zvolené varianty ze seznamu.
-         */
-        $('#modalZmenaFunguje li button').click(function(event) {
-          var text = $(this).data('text');
-          $('#frm-testForm-vysledek_testu').val(text);
-        });
-      },
-      success: function(payload) {
-        //Po úspěšném zadání výsledku testování změny.
-        if(payload.akce == 'testFormSuccess') {
-          $('#modalZmenaFunguje').modal('hide');
-          var zmena = $('.zmena[data-id="' + payload.zmena + '"]');
-          zvyraznitZmenu(zmena);
+    load: function() {
+      /**
+       * Potvrzovací dialog při kliknutí na odkaz pro smazání.
+       */
+      $('.btn-danger').click(function(event) {
+        event.preventDefault();
+        var url = $(this).attr('href');
+        var confirm_box = confirm('Opravdu?');
+        if(confirm_box) {
+          window.location = url;
         }
-        if(payload.chyba) {
-          alert(payload.chyba);
-        }
+      });
+    },
+    success: function(payload) {
+      //Po úspěšném zadání výsledku testování změny.
+      if(payload.akce == 'testFormSuccess') {
+        $('#modalZmenaFunguje').modal('hide');
+        var zmena = $('.zmena[data-id="' + payload.zmena + '"]');
+        zvyraznitZmenu(zmena);
       }
+      if(payload.chyba) {
+        alert(payload.chyba);
+      }
+    }
   });
   $.nette.init();
-
-
-  /**
-   * Po výběru verze v select boxu v menu se otevře stránka s danou verzí.
-   */
-  $('#menuForm #vyberVerze, #menuForm #vyberUzivatele, #menuForm #vyberProtokolu').change(function(event) {
-    var id = $( this ).val();
-    if(!id) return;
-    $('#menuForm').submit();
-  });
-
-  /**
-   * Posunutí změny nahoru
-   */
-  $('.posunNahoru').click(function() {
-    var zmena = $(this).closest('.zmena');
-    zvyraznitZmenu(zmena);
-    var predchudce = zmena.prevAll('.zmena:first')
-    if(predchudce.length) {
-      predchudce.before(zmena);
-      ulozitPoradi();
-    }
-  });
-
-  /**
-   * Posunutí změny dolů
-   */
-  $('.posunDolu').click(function() {
-    var zmena = $(this).closest('.zmena');
-    zvyraznitZmenu(zmena);
-    var naslednik = zmena.nextAll('.zmena:first')
-    if(naslednik.length) {
-      naslednik.after(zmena);
-      ulozitPoradi();
-    }
-  });
-
-  /**
-   * Uložení pořadí změn v protokolu
-   */
-  function ulozitPoradi() {
-    var poradi = new Array();
-    var url = $('ul#zmeny').data('url');
-    $( '.zmena' ).each(function() {
-      poradi.push($(this).data('id'));
-    });
-
-    $.get(url, { seznam: poradi.join() });
-  }
 });
